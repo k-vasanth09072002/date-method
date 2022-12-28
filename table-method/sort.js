069c26fe5329fb6627febcef30da49464cf8d4b8
k-vasanth09072002 <vasanthvasath74@gmail.com>
2022-12-28 12:33:51 +0530

    date-table-methods

---
+++ table-method/sort.js
@@ -0,0 +1,94 @@
+
+const passengers = [
+    {
+      id: 1,
+      passengerName: "Freddie Mercury",
+      isVegetarianOrVegan: false,
+      connectedFlights: 2,
+    },
+    {
+      id: 2,
+      passengerName: "Amy Winehouse",
+      isVegetarianOrVegan: true,
+      connectedFlights: 4,
+    },
+      {
+      id: 3,
+      passengerName: "Kurt Cobain",
+      isVegetarianOrVegan: true,
+      connectedFlights: 3,
+    },
+       {
+      id: 3,
+      passengerName: "Michael Jackson",
+      isVegetarianOrVegan: true,
+      connectedFlights: 1,
+    },
+  ];
+
+
+//    const connect=[];
+//        b=0;
+      
+//      let length=passengers.length;
+//        for(i=length;i>0;i--){
+//          for(j=0;j<length;j++){
+//              if(passengers[j]. connectedFlights==i){
+//                  connect[b]=passengers.sort()[j].passengerName;
+
+//                  b++;
+//              }  
+//          }
+//          }
+    
+//      console.log(connect);
+
+
+// console.log(passengers[0].connectedFlights);
+// console.log(passengers[1].connectedFlights);
+
+
+// let b=[];
+
+
+// let passenger=[7,9];
+
+// console.log(passenger);
+
+
+
+// b=[]
+// passengers.sort((s1,s2)=>s2.connectedFlights-s1.connectedFlights);
+// console.table(passengers);
+
+
+//  let obj=passengers.length;
+//  for(i=0;i<obj;i++){
+//    passengers.sort();
+//    console.log(passengers[i].  passengerName+""+passengers[i].connectedFlights); 
+  
+//  };
+
+
+
+// passengers.sort(function(x,y){
+//     return y.connectedFlights-x.connectedFlights;
+// });
+// console.log(passengers);
+
+
+
+
+
+com=passengers;
+
+function so(){
+for(i=0;i<passengers;i--){
+com.sort();
+com.reverse();
+
+}
+
+}
+console.log(com);
+ 
